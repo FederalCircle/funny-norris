29da3c62bd35a3585ea337f8b7e3e3d2ea5abb61
Alyson Maia <fvd_aqueiroz@uolinc.com>
2020-01-24 00:35:34 -0300

    Updates

---
+++ precache-manifest.170362c7ecfc1bf2a96defec73ef8570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356fc0e66b7c9d08e4a969e974874ca5",
+    "url": "/funny-norris/index.html"
+  },
+  {
+    "revision": "d986171b01132fa501ee",
+    "url": "/funny-norris/static/css/main.461f2d62.chunk.css"
+  },
+  {
+    "revision": "3e27e8ea9fa127212647",
+    "url": "/funny-norris/static/js/2.7f93452d.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/funny-norris/static/js/2.7f93452d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d986171b01132fa501ee",
+    "url": "/funny-norris/static/js/main.718ee8ad.chunk.js"
+  },
+  {
+    "revision": "390ad8dc2d2c461e6e93",
+    "url": "/funny-norris/static/js/runtime-main.2cb3f739.js"
+  }
+]);
